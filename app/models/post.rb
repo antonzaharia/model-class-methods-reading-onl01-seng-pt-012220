2beefc76245bf5858121bd1b39905b1bcccee9f5
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-15 17:39:20 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,13 @@ class Post < ActiveRecord::Base
     where(author: author_id)
   end
 
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+
   private
 
   def is_title_case
